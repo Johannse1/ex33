ec502ba430260fb7a9a0e9247beaeab0ed3aa25a
Evan <johannse@hartwick.edu>
2019-10-14 22:07:20 -0400

    update

---
+++ ex33A.py
@@ -0,0 +1,18 @@
+# evan johanns
+# ex33
+# 10/14/19
+
+i = 0
+numbers = []
+
+while i < 6:
+    print(f"at the top i is {}")
+    numbers.append(i)
+    i = i + 1
+    print("numbers now:", numbers)
+    print(f"at the bottem is {i}")
+
+print("the numbers: ")
+
+for num in numbers:
+    print(num)
